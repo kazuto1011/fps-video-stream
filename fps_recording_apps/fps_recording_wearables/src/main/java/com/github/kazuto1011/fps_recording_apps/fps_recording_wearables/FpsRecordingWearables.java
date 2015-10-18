f8add1714648bd4cad248ba7606a12e3608320a0
kazuto1011 <k_nakashima@irvs.ait.kyushu-u.ac.jp>
2015-10-18 20:44:41 +0900

    modified app ui

--- fps_recording_apps/fps_recording_wearables/src/main/java/com/github/kazuto1011/fps_recording_apps/fps_recording_wearables/FpsRecordingWearables.java
+++ fps_recording_apps/fps_recording_wearables/src/main/java/com/github/kazuto1011/fps_recording_apps/fps_recording_wearables/FpsRecordingWearables.java
@@ -1,10 +1,15 @@
 package com.github.kazuto1011.fps_recording_apps.fps_recording_wearables;
 
+import android.content.Context;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
@@ -17,13 +22,16 @@ import java.net.URI;
 public class FpsRecordingWearables extends RosActivity
 {
     private final String TAG = "FpsRecordingWearables";
-    private Camera mCamera;
     protected int numberOfCameras;
+    private Camera camera;
     private int cameraId = 0;
     private RosCameraPreviewView rosCameraPreviewView;
+    private TextView textView;
+    private Handler handler;
+    private Context context = this;
 
     public FpsRecordingWearables() {
-        super("FpsRecordingWearables", "FpsRecordingWearables", URI.create("http://192.168.4.77:11311"));
+        super("FpsRecordingWarables", "FpsRecordingWearables", URI.create("http://192.168.4.77:11311"));
     }
 
     @Override
@@ -36,6 +44,7 @@ public class FpsRecordingWearables extends RosActivity
         setContentView(R.layout.main);
 
         rosCameraPreviewView = (RosCameraPreviewView)findViewById(R.id.camera_view);
+        textView = (TextView)findViewById(R.id.rec_info);
 
         numberOfCameras = Camera.getNumberOfCameras();
         if (numberOfCameras > 1) {
@@ -43,18 +52,37 @@ public class FpsRecordingWearables extends RosActivity
             rosCameraPreviewView.releaseCamera();
             rosCameraPreviewView.setCamera(Camera.open(cameraId));
         }
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                Toast.makeText(context, (String) msg.obj, Toast.LENGTH_LONG).show();
+            }
+        };
     }
 
     @Override
     protected void init(NodeMainExecutor nodeMainExecutor) {
-        Log.d(TAG, "init");
-        cameraId = 0;
-        rosCameraPreviewView.setCamera(Camera.open(cameraId));
+        Log.d(TAG, "init camera ID:" + cameraId);
+        camera = Camera.open(cameraId);
+
+        int fps[] = new int[2];
+        camera.getParameters().getPreviewFpsRange(fps);
+
+        String info = "";
+        info += "Frame rate: " + String.valueOf(fps[1]/1000);
+        info += "\nWidth: " + camera.getParameters().getPreviewSize().width;
+        info += "\nHeight: " + camera.getParameters().getPreviewSize().height;
+
+        Message msg = handler.obtainMessage(0, info);
+        handler.sendMessage(msg);
+
+        rosCameraPreviewView.setCamera(camera);
 
         NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostAddress());
         nodeConfiguration.setMasterUri(getMasterUri());
 
         nodeMainExecutor.execute(rosCameraPreviewView, nodeConfiguration.setNodeName("M100"));
     }
-
 }
